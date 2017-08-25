66261103561801eb822249c719fb6337e7b6ac29
Takahiro Hirata <takahiro@hiratanoMacBook-Pro.local>
2017-08-25 11:56:12 +0900

    add lint command end fix lint error

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,14 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
+const MidFreeCoinCrowdsale = artifacts.require('/MidFreeCoinCrowdsale.sol');
 
-var MidFreeCoinCrowdsale = artifacts.require("./MidFreeCoinCrowdsale.sol");
-
-// module.exports = function(deployer) {
-  // deployer.deploy(ConvertLib);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
-// };
-
-module.exports = function deployContract(deployer, network, accounts) {
+module.exports = function deployContract(deployer) {
   // クラウドセールが始まるブロックを現在のブロックの2ブロック後に設定
-  const startBlock = web3.eth.blockNumber + 2 
+  const startBlock = web3.eth.blockNumber + 2;
   // 終了させるブロックの番号→300だと1時間ちょっと
-  const endBlock = startBlock + 300  
+  const endBlock = startBlock + 300;
   // EtherとMidFreeCoinの価値の比率
-  const rate = new web3.BigNumber(1000) 
-  // ファンドのアドレス　マルチシグのものの方がセキュリティ的に良い
-  const wallet = web3.eth.accounts[0] 
+  const rate = new web3.BigNumber(1000);
+  // ファンドのアドレス→マルチシグのものの方がセキュリティ的に良い
+  const wallet = web3.eth.accounts[0];
 
-  deployer.deploy(MidFreeCoinCrowdsale, startBlock, endBlock, rate, wallet)
-}
+  deployer.deploy(MidFreeCoinCrowdsale, startBlock, endBlock, rate, wallet);
+};
