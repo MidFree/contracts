cb81c92e405f8e153082aba6e58575aceca50434
Takahiro Hirata <takahiro096041@gmail.com>
2017-09-25 09:56:04 +0900

    add private net setting

--- truffle.js
+++ truffle.js
@@ -17,5 +17,12 @@ module.exports = {
       gas: 4400000,
       gasPrice: 2100000000000,
     },
+    private: {
+      host: 'localhost', // set IP for private network
+      port: 8545,
+      network_id: '10', // Match any network id
+      gas: 4400000,
+      gasPrice: 2100000000000,
+    },
   },
 };
